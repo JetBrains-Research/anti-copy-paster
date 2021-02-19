3c781d825c21320152099b929157c0e2f0d0a8e9
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2021-02-19 14:23:27 +0300

    Delete module with experiments and module with dataset analysis

--- RefactoringsExperiments/src/main/java/ru/hse/kirilenko/refactorings/ExtractionConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.hse.kirilenko.refactorings;
-
-public class ExtractionConfig {
-    public static boolean extractDirectly = true;
-    public static boolean onlyExtractedOperation = true;
-    public static boolean noSubfolders = false;
-    public static boolean parseJava = true;
-    public static int nFeatures = 117;
-}
--- RefactoringsExperiments/src/main/java/ru/hse/kirilenko/refactorings/csv/models/ICSVItem.java
+++
@@ -1,7 +0,0 @@
-package ru.hse.kirilenko.refactorings.csv.models;
-
-public interface ICSVItem {
-    int getId();
-    double getValue();
-    void setValue(double newValue);
-}
