b198748aea9883b98b52fb6df2ee7bd5cc322659
tap5136 <tap5136@g.rit.edu>
2023-03-12 18:31:43 -0400

    FEAT-20 - renaming model class

--- src/main/java/org/jetbrains/research/anticopypaster/models/UserSettingsModel.java
+++ src/main/java/org/jetbrains/research/anticopypaster/models/UserSettingsModel.java
@@ -2,7 +2,7 @@ package org.jetbrains.research.anticopypaster.models;
 
 import org.jetbrains.research.extractMethod.metrics.features.FeaturesVector;
 
-public class UserDefinedModel extends PredictionModel{
+public class UserSettingsModel extends PredictionModel{
 
     @Override
     public float predict(FeaturesVector featuresVector){
