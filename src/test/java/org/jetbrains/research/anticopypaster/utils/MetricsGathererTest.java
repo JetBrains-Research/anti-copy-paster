a860aec6aabb3df8cbb0c11aaa16c24929943a8e
ttp2542 <ttp2542@g.rit.edu>
2023-03-12 14:13:25 -0400

    FEAT-33 cleaned up imports, ensured test files wouldn't be gotten, and commented in some test cases in the test file

--- src/test/java/org/jetbrains/research/anticopypaster/utils/MetricsGathererTest.java
+++ src/test/java/org/jetbrains/research/anticopypaster/utils/MetricsGathererTest.java
@@ -1,5 +1,8 @@
 package org.jetbrains.research.anticopypaster.utils;
 
 public class MetricsGathererTest {
-
+    /**
+     * Test cases to implement: Normal case, one with files that start or end with 'test',
+     * make sure methods are gotten
+     */
 }
