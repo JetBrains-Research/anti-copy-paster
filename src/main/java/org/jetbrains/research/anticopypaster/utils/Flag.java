241fcc27cd5bb4145d995bd62c714e56f942e7ac
tap5136 <tap5136@g.rit.edu>
2023-03-14 17:17:19 -0400

    FEAT-37 - Testing for the Flag utils

--- src/main/java/org/jetbrains/research/anticopypaster/utils/Flag.java
+++ src/main/java/org/jetbrains/research/anticopypaster/utils/Flag.java
@@ -19,9 +19,24 @@ public abstract class Flag{
 
     public Flag(List<FeaturesVector> featuresVectorList){
         this.featuresVectorList = featuresVectorList;
+        this.metricQ1=0;
+        this.metricQ2=0;
+        this.metricQ3=0;
     }
 
+    /**
+    Takes a SORTED list and generates/sets the Q1-3 values based on a box plot
+    of those metric values
+     */
     protected void boxPlotCalculations(ArrayList<Float> data){
+
+        if(data == null || data.size() == 0){
+            metricQ1=0;
+            metricQ2=0;
+            metricQ3=0;
+            return;
+        }
+
         float q1;
         if (data.size() % 2 == 0) {
             q1 = (data.get(data.size()/4 - 1) + data.get(data.size()/4)) / 2;
@@ -45,6 +60,18 @@ public abstract class Flag{
             q3 = data.get(data.size()*3/4);
         }
         metricQ3 = q3;
+    }
+
+    public float getMetricQ1(){
+        return this.metricQ1;
+    }
+
+    public float getMetricQ2(){
+        return this.metricQ2;
+    }
+
+    public float getMetricQ3(){
+        return this.metricQ3;
     }
 
     public int changeSensitivity(int sensitivity){
--- src/test/java/org/jetbrains/research/anticopypaster/utils/FlagTest.java
+++ src/test/java/org/jetbrains/research/anticopypaster/utils/FlagTest.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.*;
 public class FlagTest {
 
     /**
-    Local testing implementation for this class to test the utilities
+    Inner, nonspecific testing implementation for this class to test the shared utilities
      */
     class TestingFlag extends Flag{
 
@@ -75,5 +75,91 @@ public class FlagTest {
         assertEquals(newSens, 0);
     }
 
+    @Test 
+    public void testBoxPlotNullList(){
+        testFlag.boxPlotCalculations(null);
+        assertEquals(0, testFlag.getMetricQ1(), 0);
+        assertEquals(0, testFlag.getMetricQ2(), 0);
+        assertEquals(0, testFlag.getMetricQ3(), 0);
+    }
+
+    @Test 
+    public void testBoxPlotEmptyList(){
+        ArrayList emptyList = new ArrayList<Float>();
+        testFlag.boxPlotCalculations(emptyList);
+        assertEquals(0, testFlag.getMetricQ1(), 0);
+        assertEquals(0, testFlag.getMetricQ2(), 0);
+        assertEquals(0, testFlag.getMetricQ3(), 0);
+    }
+
+    @Test 
+    public void testBoxPlotSameNumberFourValuesList(){
+        ArrayList fourValueSameNumberFloatList = new ArrayList<Float>();
+        
+        fourValueSameNumberFloatList.add((float)1.0);
+        fourValueSameNumberFloatList.add((float)1.0);
+        fourValueSameNumberFloatList.add((float)1.0);
+        fourValueSameNumberFloatList.add((float)1.0);
+
+        Collections.sort(fourValueSameNumberFloatList);
+
+        testFlag.boxPlotCalculations(fourValueSameNumberFloatList);
+        assertEquals(1, testFlag.getMetricQ1(), 0);
+        assertEquals(1, testFlag.getMetricQ2(), 0);
+        assertEquals(1, testFlag.getMetricQ3(), 0);
+    }
+
+    @Test 
+    public void testBoxPlotSameNumberFiveValuesList(){
+        ArrayList fiveValueSameNumberFloatList = new ArrayList<Float>();
+        
+        fiveValueSameNumberFloatList.add((float)1.0);
+        fiveValueSameNumberFloatList.add((float)1.0);
+        fiveValueSameNumberFloatList.add((float)1.0);
+        fiveValueSameNumberFloatList.add((float)1.0);
+        fiveValueSameNumberFloatList.add((float)1.0);
+
+        Collections.sort(fiveValueSameNumberFloatList);
+
+        testFlag.boxPlotCalculations(fiveValueSameNumberFloatList);
+        assertEquals(1, testFlag.getMetricQ1(), 0);
+        assertEquals(1, testFlag.getMetricQ2(), 0);
+        assertEquals(1, testFlag.getMetricQ3(), 0);
+    }
+
+    @Test 
+    public void testBoxPlotDifferentNumberFourValuesList(){
+        ArrayList fourValueDifferentNumberFloatList = new ArrayList<Float>();
+        
+        fourValueDifferentNumberFloatList.add((float)1.0);
+        fourValueDifferentNumberFloatList.add((float)2.0);
+        fourValueDifferentNumberFloatList.add((float)3.0);
+        fourValueDifferentNumberFloatList.add((float)4.0);
+
+        Collections.sort(fourValueDifferentNumberFloatList);
+
+        testFlag.boxPlotCalculations(fourValueDifferentNumberFloatList);
+        assertEquals(1.5, testFlag.getMetricQ1(), 0);
+        assertEquals(2.5, testFlag.getMetricQ2(), 0);
+        assertEquals(3.5, testFlag.getMetricQ3(), 0);
+    }
+
+    @Test 
+    public void testBoxPlotDifferentNumberFiveValuesList(){
+        ArrayList fiveValueDifferentNumberFloatList = new ArrayList<Float>();
+        
+        fiveValueDifferentNumberFloatList.add((float)1.0);
+        fiveValueDifferentNumberFloatList.add((float)2.0);
+        fiveValueDifferentNumberFloatList.add((float)3.0);
+        fiveValueDifferentNumberFloatList.add((float)4.0);
+        fiveValueDifferentNumberFloatList.add((float)5.0);
+
+        Collections.sort(fiveValueDifferentNumberFloatList);
+
+        testFlag.boxPlotCalculations(fiveValueDifferentNumberFloatList);
+        assertEquals(2, testFlag.getMetricQ1(), 0);
+        assertEquals(3, testFlag.getMetricQ2(), 0);
+        assertEquals(4, testFlag.getMetricQ3(), 0);
+    }
 
 }
